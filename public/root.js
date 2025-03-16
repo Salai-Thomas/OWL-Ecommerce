b20842cc3950bdeccc6e49ff966c7646da44a923
SalaiThomas <salaithomas582@gmail.com>
2025-03-16 19:15:31 +0630

    using inline template with xml helper

--- public/root.js
+++ public/root.js
@@ -1,5 +1,8 @@
-import { Component } from "@odoo/owl";
+import { Component,xml } from "@odoo/owl";
 
 export class Root extends Component {
-  static template = "Root";
+  // static template = "Root";
+  static template = xml`
+    <h2>Welcome From OWL!</h2>
+  `
 }
