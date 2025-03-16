5279fc8b99ad9d1380090a90156e3a01e326cd61
SalaiThomas <salaithomas582@gmail.com>
2025-03-16 19:28:05 +0630

    subComponent

--- public/root.js
+++ public/root.js
@@ -1,8 +1,17 @@
 import { Component,xml } from "@odoo/owl";
 
+export class Child extends Component{
+  static template = xml`
+    <h4>That Is Child Component</h4>
+  `
+}
+
 export class Root extends Component {
   // static template = "Root";
   static template = xml`
     <h2>Welcome From OWL!</h2>
+    <Child/>
   `
+
+  static components = {Child};
 }
